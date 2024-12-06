598108862c5201b5e096d16650c5eea52ba3e007
Mert Akyazi <mertakyazi9@gmail.com>
2024-12-06 03:06:19 +0300

    fix(auth.tsx): correct currentChain request handling to ensure proper network validation

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -34,9 +34,10 @@ export async function switchChain(chainId: `mina:${string}`) {
 
     return chainId;
   } else {
-    const currentChain = await mina.request({
+    const currentChain = (await mina.request({
       method: "mina_chainId",
-    });
+    })
+    ).result;
     if (currentChain !== "29936104443aaf264a7f0192ac64b1c7173198c1ed404c1bcff5e562e05eb7f6") {
       throw new Error(`This network is not supported. Switch to devnet to continue.`);
     }
